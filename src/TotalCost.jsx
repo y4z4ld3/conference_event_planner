29895c8f4244d3a50a5804424cff4831f6958ebd
Yazalde Amaya <yazalde.amaya@gmail.com>
2025-06-04 15:52:34 -0400

    Finales

--- src/TotalCost.jsx
+++ src/TotalCost.jsx
@@ -2,8 +2,7 @@ import React, { useState, useEffect } from 'react';
 import "./TotalCost.css";
 
 const TotalCost = ({ totalCosts, ItemsDisplay }) => {
-  
-
+  const total_amount = totalCosts.venue + totalCosts.av + totalCosts.meals;
   return (
     <div className="pricing-app">
       <div className="display_box">
